901c8d00c0844fc24c79f3311149a8417341dd4e
Andrew Tian <113626415+Zeldoriz@users.noreply.github.com>
2023-11-04 15:25:58 +0800

    Updated all files

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-$(".action-btn").on('click', function () {
-    let menu = $(".dropdown-menu"),
-        btn = $(this),
-        icon = $(".action-btn i");
-
-    if (menu.hasClass("hidden")) {
-        icon.removeClass("fa-chevron-down");
-        icon.addClass("fa-chevron-up");
-        btn.addClass("border");
-        menu.removeClass("hidden");
-    } else {
-        icon.removeClass("fa-chevron-up");
-        icon.addClass("fa-chevron-down");
-        btn.removeClass("border");
-        menu.addClass("hidden");
-    }
-});
-
 let tl = ".top-left",
     tm = ".top-mid",
     tr = ".top-right",
@@ -32,6 +14,58 @@ let turnCounter = 1,
     player1Wins = 0,
     player2Wins = 0,
     ties = 0;
+
+$(".action-btn").on('click', function () {
+    let menu = $(".dropdown-menu"),
+        btn = $(this),
+        icon = $(".action-btn i");
+
+    if (menu.hasClass("hidden")) {
+        icon.removeClass("fa-chevron-down");
+        icon.addClass("fa-chevron-up");
+        btn.addClass("border");
+        menu.toggleClass("hidden");
+    } else {
+        icon.removeClass("fa-chevron-up");
+        icon.addClass("fa-chevron-down");
+        btn.removeClass("border");
+    }
+
+    menu.toggleClass("dd-menu-down")
+    menu.toggleClass("dd-menu-up")
+    if (menu.hasClass("dd-menu-up")) {
+        setTimeout(() => {
+            menu.toggleClass("hidden");
+        }, 300);
+    }
+
+    $(".reset-btn").toggleClass("reset-btn-animate")
+    $(".new-round-btn").toggleClass("hidden")
+    setTimeout(function () {
+        $(".new-round-btn").toggleClass("new-round-btn-animate")
+        $(".new-round-btn").toggleClass("hidden")
+    }, 100);
+});
+
+$(".reset-btn").on('click', function () {
+    resetGame()
+})
+
+$(".new-round-btn").on('click', function () {
+    resetGame()
+    turnCounter = 1,
+        player1Wins = 0,
+        player2Wins = 0,
+        ties = 0;
+    updateScore()
+
+})
+
+function updateScore() {
+    $(".player1 p").html(player1Wins + " Wins")
+    $(".player2 p").html(player2Wins + " Wins")
+    $(".ties p").html(ties + " Ties")
+}
 
 function p1Turn() {
     $(".turn p").html("Player 1, you're up!")
@@ -61,24 +95,42 @@ function resetGame() {
         $(grid + " i").removeClass("fa-x")
         $(grid + " i").removeClass("yellow")
         $(grid + " i").removeClass("turquoise")
-
         p1Turn()
     });
-    turnCounter = 0
+    turnCounter = 1
 }
+
+function roundOver() {
+    $(".turn p").html("Round Over!").css("color", "white")
+    $(".turn i").removeClass("fa-o")
+    $(".turn i").removeClass("fa-x")
+    $(".turn i").addClass("fa-undo")
+    $(".round-over-screen").toggleClass("hidden")
+    toggle.toggleClass("hidden")
+}
+
+$(".next-round-btn").on('click', function () {
+    resetGame()
+    $(".round-over-screen").toggleClass("hidden")
+    $(".turn i").removeClass("fa-undo")
+})
 
 function winCond(player) {
     if (player == 1) {
         console.log("Player 1 Wins")
+        $(".round-over-prompt p").html("Player 1 Wins!")
         player1Wins++
     } else if (player == 2) {
         console.log("Player 2 Wins")
+        $(".round-over-prompt p").html("Player 2 Wins!")
         player2Wins++
     } else {
         console.log("Tie")
+        $(".round-over-prompt p").html("Tie!")
         ties++
     }
-    resetGame()
+    updateScore()
+    roundOver()
 }
 
 (grid).forEach(grid => {
@@ -147,10 +199,6 @@ function winCond(player) {
             } else if (turnCounter == 9) {
                 winCond(3)
             }
-
-            $(".player1 p").html(player1Wins + " Wins")
-            $(".player2 p").html(player2Wins + " Wins")
-            $(".ties p").html(ties + " Ties")
             turnCounter++
         }
     })
